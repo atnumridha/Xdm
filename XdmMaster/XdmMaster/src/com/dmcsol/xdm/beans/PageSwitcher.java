0ffba8552c06379bad92bafac435cfb0d5e28fab
Atanu.Mridha <Atanu.Mridha@IN2271123W1.ey.net>
2018-04-22 18:04:16 +0530

    Working Copy without Navigation

---
+++ XdmMaster/XdmMaster/src/com/dmcsol/xdm/beans/PageSwitcher.java
@@ -0,0 +1,20 @@
+package com.dmcsol.xdm.beans;
+
+import java.io.Serializable;
+
+import oracle.adf.controller.TaskFlowId;
+
+public class PageSwitcher implements Serializable {
+    private String taskFlowId = "/WEB-INF/task-overview-edit-flow.xml#task-overview-edit-flow";
+
+    public PageSwitcher() {
+    }
+
+    public TaskFlowId getDynamicTaskFlowId() {
+        return TaskFlowId.parse(taskFlowId);
+    }
+
+    public void setDynamicTaskFlowId(String taskFlowId) {
+        this.taskFlowId = taskFlowId;
+    }
+}
